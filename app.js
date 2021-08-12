29685360ef264d4c8b7e52451071813e57ec556f
Dylan Floyd <floyd800@gmail.com>
2021-08-11 17:43:35 -0700

    add basic site and functionality

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 // import functions and grab DOM elements
+let catPic = document.getElementById('cat-pic');
+let catSound = document.getElementById('cat-sound');
+
+let dogPic = document.getElementById('dog-pic');
+let dogSound = document.getElementById('dog-sound');
+
+let horsePic = document.getElementById('horse-pic');
+let horseSound = document.getElementById('horse-sound');
+
+let display = document.getElementById('animal-display');
+
+let meow = () => {
+  catSound.play();
+  display.textContent="The cat goes: 'meow'";  
+}
+
+let woof = () => {
+  dogSound.play();
+  display.textContent="The dog goes: 'woof'";
+}
+
+let HUHUHUHU = () => {
+  horseSound.play();
+  display.textContent="The horse goes: 'HUHUHUHU'";
+}
 
 // initialize state
 
@@ -6,3 +31,18 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+catPic.addEventListener('click', meow);
+
+dogPic.addEventListener('click', woof);
+
+horsePic.addEventListener('click', HUHUHUHU);
+
+document.addEventListener('keyup', (e) => {
+  if(e.key === "c") {
+    meow();
+  } else if(e.key === "d") {
+    woof();
+  } else if(e.key === "h") {
+    HUHUHUHU();
+  }
+});
